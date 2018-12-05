132d2310cfca4e928c0a922fcdc7b6163968aaa1
bamchoh <bamchoh@gmail.com>
2018-12-06 00:47:49 +0900

    Add test for controller

--- app/controllers/web_time_card_controller.rb
+++ app/controllers/web_time_card_controller.rb
@@ -1,26 +1,26 @@
 class WebTimeCardController < ApplicationController
   def index
-    @cards = Card.all
+    @cards = Card.order("datetime")
     unless @cards.empty?
-	    last = @cards.last
-	    if last.status.to_s == "start"
-		    @status = "end"
-	    else
-		    @status = "start"
-	    end
+      last = @cards.last
+      if last.status.to_s == "start"
+        @worktype = "end"
+      else
+        @worktype = "start"
+      end
     else
-	    @status = "start"
+      @worktype = "start"
     end
   end
 
   def start
-	  @cards = Card.new(post_params)
-	  @cards.save
-	  redirect_to "/index"
+    @cards = Card.new(post_params)
+    @cards.save
+    redirect_to "/index"
   end
 
   def post_params
-	  params["card"]["datetime"] = DateTime.now
-	  params.require(:card).permit(:datetime, :status)
+    params["card"]["datetime"] = DateTime.now
+    params.require(:card).permit(:datetime, :status)
   end
 end
--- test/controllers/web_time_card_controller_test.rb
+++ test/controllers/web_time_card_controller_test.rb
@@ -1,9 +1,38 @@
 require 'test_helper'
+require 'pp'
 
 class WebTimeCardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get web_time_card_index_url
     assert_response :success
+    assert_equal 8, Card.all.size
   end
 
+  test "get last status as start" do
+    get web_time_card_index_url
+    type = @controller.instance_variable_get(:@worktype)
+    assert_equal "start", type
+  end
+
+  test "get last status as end" do
+    post "/start", :params => { :card => {:status => "start" } }
+    get web_time_card_index_url
+    type = @controller.instance_variable_get(:@worktype)
+    assert_equal "end", type
+  end
+
+  test "zero record" do
+    Card.destroy_all
+    get web_time_card_index_url
+    type = @controller.instance_variable_get(:@worktype)
+    assert_equal "start", type
+  end
+
+  test "one record" do
+    Card.destroy_all
+    post "/start", :params => { :card => {:status => "start" } }
+    get web_time_card_index_url
+    type = @controller.instance_variable_get(:@worktype)
+    assert_equal "end", type
+  end
 end
